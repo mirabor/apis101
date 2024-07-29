086ba9903e0b8a20f92506c22df08dced5685971
mirabor <mirawithyu@gmail.com>
2024-07-29 14:15:02 -0400

    set up

--- reference/starter-movies-api/index.js
+++ reference/starter-movies-api/index.js
@@ -7,6 +7,12 @@ app.get("/about", (req, res) => {
 	res.send("This is an API service for CRUD actions on movies resources.");
 });
 
+// the {name} value is dynamic based on the URL provided.
+app.get("/about/:name", (req, res) => {
+	const name = req.params.name;
+	res.send("This is an API service for CRUD actions on a movies resource...for you " + name);
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
 });
