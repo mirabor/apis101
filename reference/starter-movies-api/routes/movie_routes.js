5d4553d6ffbdf55129df23625b340866a4ebb0b9
mirabor <mirawithyu@gmail.com>
2024-07-29 14:50:33 -0400

    controller, router

---
+++ reference/starter-movies-api/routes/movie_routes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const {
+	getAllMovies,
+	getMovieById,
+	createMovie,
+	deleteMovieById,
+	updateMovieById,
+} = require("../controllers/movies_controller");
+
+router.get("/:id", (req, res) => {
+	const movieId = Number(req.params.id);
+	const movie = getMovieById(movieId);
+	res.json(movie);
+});
+
+router.post("/", (req, res) => {
+	const newMovie = {
+		name: req.body.name,
+		genre: req.body.genre,
+		img: req.body.img,
+	};
+
+	createMovie(newMovie);
+	res.status(201).json(newMovie);
+});
+
+router.put("/:id", (req, res) => {
+	movieId = Number(req.params.id);
+	const updatedMovie = {
+		id: movieId,
+		name: req.body.name,
+		genre: req.body.genre,
+		img: req.body.img,
+	};
+	updateMovieById(movieId, updatedMovie);
+	res.json(updatedMovie);
+});
+
+router.delete("/:id", (req, res) => {
+	movieId = Number(req.params.id);
+	res.json(deleteMovieById(movieId));
+});
+
+module.exports = router;
